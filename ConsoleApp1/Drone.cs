b1a902c32d618131fe4d9f712b35103f2e8417fc
MarcoLGP <marcolucagp@gmail.com>
2023-02-07 16:30:43 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Drone.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Drone
+    {
+        private double? _Altura { get; set; }
+        private double? _Angulo { get; set; }
+        private double? _Velocidade { get; set; }
+        private string? _Status { get; set; }
+
+        public Drone(double altura)
+        {
+            _Altura = altura;
+        }
+
+        public void DefinirAltura(double altura)
+        {
+            if (altura > 25 || altura < 0.5) 
+            {
+                throw new Exception("Altura inválida, deve estar entre 0,5m e 20m");
+            }
+            _Altura = altura;
+        }
+
+        public void AumentarAltura()
+        {
+            if (_Altura > 25)
+            {
+                throw new Exception("Altura máxima já atingida");
+            }
+            _Altura += 0.5;
+        }
+
+        public void DefinirAngulo(double angulo)
+        {
+            if (angulo > 360 || angulo < 0) 
+            {
+                throw new Exception("Ângulo inválido");
+            }
+            _Angulo = angulo;
+        }
+
+        public void MoverEsquerda()
+        {
+            if (_Angulo - 5 <= 0)
+            {
+                _Angulo = (_Angulo * -1) - 360;
+            }
+            _Angulo -= 5;
+        }
+
+        public void MoverDireita()
+        {
+            if (_Angulo + 5 >= 360)
+            {
+                _Angulo -= 360;
+            }
+            else
+            {
+                _Angulo += 5;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    public class ConsoleApp1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow");
+            Exercicios.Imprimir();
+            Exercicios.Soma(52, 35);
+        }
+    }
+}
